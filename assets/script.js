93395a41e6fa5331e7124c4c749d6337859549f9
0Clark30 <austinlaw.clark@outlook.com>
2024-02-19 19:41:02 -0600

    added to layout

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,12 @@
-var APIkey = "20ec5ae9470963b8c0b41f91194289e2"
+var apiKey = "20ec5ae9470963b8c0b41f91194289e2"
 var city;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+city = "Zocca"
+fetch(queryURL)
+.then(function (response){
+    return response.json();
+})
+.then(function(data){
+    console.log(data)
+})
