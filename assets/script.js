2b4e7f7e9b4cdb56553fa748346a81defec523f8
0Clark30 <austinlaw.clark@outlook.com>
2024-02-27 18:13:24 -0600

    added

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,43 @@
-var apiKey = "20ec5ae9470963b8c0b41f91194289e2"
-var city;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+var apiKey = "20ec5ae9470963b8c0b41f91194289e2";
 
-city = "Zocca"
-fetch(queryURL)
-.then(function (response){
+var searchBTN = document.getElementsByClassName("btn")[0];
+var cityInput = document.getElementById("cityInput");
+
+
+
+searchBTN.addEventListener('click', function() {
+    let city = cityInput.value;
+    return console.log(city) 
+})
+
+const atlanta = {
+  name: "Atlanta",
+  lat: 33.749,
+  lon: -84.388
+};
+
+const houston = {
+  name: "Houston",
+  lat: 29.7604,
+  lon: -95.3698
+};
+
+const seattle = {
+  name: "Seattle",
+  lat: 47.6062,
+  lon: -122.3321
+};
+
+
+var queryURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+
+ fetch(queryURL)
+  .then(function (response) {
     return response.json();
-})
-.then(function(data){
-    console.log(data)
-})
+  })
+  .then(function (data) {
+    console.log(data);
+  });   
+
+
