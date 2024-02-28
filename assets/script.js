ac5c325f3153c1c5355bfca4e792ca7dfdadf4a2
0Clark30 <austinlaw.clark@outlook.com>
2024-02-27 19:52:01 -0600

    added to js

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,9 @@ var apiKey = "20ec5ae9470963b8c0b41f91194289e2";
 
 var searchBTN = document.getElementsByClassName("btn")[0];
 var cityInput = document.getElementById("cityInput");
-
-
-
-searchBTN.addEventListener('click', function() {
-    let city = cityInput.value;
-    return console.log(city) 
-})
-
+let lat;
+let lon;
+let city;
 const atlanta = {
   name: "Atlanta",
   lat: 33.749,
@@ -27,6 +22,26 @@ const seattle = {
   lat: 47.6062,
   lon: -122.3321
 };
+
+searchBTN.addEventListener('click', function() {
+    city = cityInput.value;
+    return city
+})
+
+
+console.log(city)
+if (city = atlanta){
+    lat = atlanta.lat
+    lon = atlanta.lon
+} else if (city = houston){
+    lat = houston.lat
+    lon = houston.lon
+}else {
+    lat = seattle.lat
+    lon =seattle.lon
+}
+
+
 
 
 var queryURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
